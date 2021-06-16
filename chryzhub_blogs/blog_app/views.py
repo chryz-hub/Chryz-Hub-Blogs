c720805313a37302441dc2f9af3166d32c64cdee
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2021-06-16 16:07:19 +0100

    fixed a bug

--- chryzhub_blogs/blog_app/views.py
+++ chryzhub_blogs/blog_app/views.py
@@ -12,9 +12,15 @@ from .forms import PostForm, CategoryForm
 
 def LikeView(request, pk):
     post =get_object_or_404(Post, id=request.POST.get('post_id'))
-    post.likes.add(request.user)
+    liked = False
+    if post.likes.filter(id=request.user.id).exists
+        post.likes.remove(request.user)
+        liked = False
+    else:
+        post.likes.add(request.user)
+        liked = True
     return HttpResponseRedirect(reverse('blog_detail', args=[str(pk)]))
-    
+
 
 class BlogList(ListView):
     model = Post
