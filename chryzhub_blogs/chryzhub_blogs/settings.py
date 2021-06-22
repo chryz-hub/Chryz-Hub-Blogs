608ab936e36596559093d65ba4e657dac0cb0c0e
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2021-06-22 09:07:44 +0100

    adding header image to all blogs

--- chryzhub_blogs/chryzhub_blogs/settings.py
+++ chryzhub_blogs/chryzhub_blogs/settings.py
@@ -1,5 +1,3 @@
-from .sec import *
-
 """
 Django settings for chryzhub_blogs project.
 
@@ -11,8 +9,10 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
+from .sec import *
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog_app',
-    'ckeditor',    
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
@@ -120,12 +120,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = "blog_list"
 LOGOUT_REDIRECT_URL = "blog_list"
