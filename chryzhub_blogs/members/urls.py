e55498eef5fd4b51bcb8a76e82e2fa3702aa71fb
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2021-08-05 11:39:22 +0100

    commit

--- chryzhub_blogs/members/urls.py
+++ chryzhub_blogs/members/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path('register/', CreateAccount.as_view(), name='register'),
     path('edit_profile/', UpdateAccount.as_view(), name='update_account'),
-    path('password/', PasswordsChangeView.as_view(template_name='registration/change-password.html'), name='change_passowrd'),
+    path('password/', PasswordsChangeView.as_view(template_name='registration/change-password.html'), name='change_password'),
     path('password_success', views.password_success, name = 'password_success'),
     path('<int:pk>/profile/', ShowProfilePageView.as_view(), name='show_profile_page'),
     path('<int:pk>/edit_profile_page/', EditProfilePageView.as_view(), name='edit_profile_page'),
