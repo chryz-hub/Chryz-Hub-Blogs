6ce59ebcf2bddd88b2c498d412322de55f911f7d
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2021-08-13 23:50:26 +0100

    commit

--- chryzhub_blogs/chryzhub_blogs/settings.py
+++ chryzhub_blogs/chryzhub_blogs/settings.py
@@ -56,6 +56,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -154,6 +156,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
