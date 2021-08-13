a9bd1a5e41cea4c44f2021c025070464a46036db
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2021-08-14 00:12:13 +0100

    push

--- chryzhub_blogs/chryzhub_blogs/settings.py
+++ chryzhub_blogs/chryzhub_blogs/settings.py
@@ -163,7 +163,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 STATIC_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, '/static/')
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
