30384a57edd3063c46d5af0c749bd1fe401fc6a8
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2021-08-14 00:19:01 +0100

    push

--- chryzhub_blogs/chryzhub_blogs/settings.py
+++ chryzhub_blogs/chryzhub_blogs/settings.py
@@ -158,18 +158,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
+MEDIA_URL = '/media/'
 
 # STATIC_DIRS = [
-#     os.path.join(BASE_DIR, '/static/')
+#     os.path.join(BASE_DIR, '/static')
 # ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static'),
+    os.path.join(BASE_DIR,'/static/'),
 ]
 
 # Default primary key field type
