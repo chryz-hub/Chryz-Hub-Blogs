4e243d9385ff7eb9a2c93ce549e48aa326655edc
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2021-08-27 01:49:29 +0100

    commit

--- chryzhub_blogs/settings.py
+++ chryzhub_blogs/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-# DEBUG = config('DEBUG')
+DEBUG = True
 
 # SECRET_KEY = config('SECRET_KEY')
 
