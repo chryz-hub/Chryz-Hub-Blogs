e4942c1a4d3d0d2fa603a1442e16f2f587d9610e
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2021-08-27 02:46:28 +0100

    commit

--- chryzhub_blogs/settings.py
+++ chryzhub_blogs/settings.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 import django_heroku
 import cloudinary_storage
-# from decouple import config
+from decouple import config
 
 
 
@@ -196,4 +196,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
+EMAIL_HOST_USER=config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD=config('EMAIL_HOST_PASSWORD')
