53b20d50816d37b34cb4d199073a92bc6c5bab66
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-09-26 13:48:03 +0100

    push

--- chryzhub_blogs/settings.py
+++ chryzhub_blogs/settings.py
@@ -106,12 +106,6 @@ WSGI_APPLICATION = 'chryzhub_blogs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
     'default': {
