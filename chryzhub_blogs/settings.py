ee32b560a11c0d0965edaa084d36f5db45963729
chryzcode.dev <alabaolanrewaju13@gmail.com>
2022-09-27 15:55:13 +0100

    Update settings.py

--- chryzhub_blogs/settings.py
+++ chryzhub_blogs/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = str(os.getenv("SECRET_KEY"))
 DEBUG = config("DEBUG", default=False, cast=bool)
 
 
-ALLOWED_HOSTS = ['chryzhubblogs.herokuapp.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['chryzhub-blogs.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
